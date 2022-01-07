b8ee9c809c4e34ff1d18562b69d636645f874333
littledragoner <1559897062@qq.com>
2022-01-07 17:53:46 +0800

    build: gin

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello 哈哈")
+	r := gin.New()
 
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World",
+		})
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    http.StatusNotFound,
+				"error_message": "路由未定义,请确认 url 和请求方法是否正确.",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
